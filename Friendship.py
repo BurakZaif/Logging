2dd1de7d60d52a516ef2f60cadda9727ad734556
BurakZaif <burakzaiff@gmail.com>
2023-08-24 16:18:56 +0300

    One more example file with class

---
+++ Friendship.py
@@ -0,0 +1,26 @@
+import logging
+
+logging.basicConfig(filename="friend.log", level=logging.INFO, format= '%(levelname)s:%(name)s:%(message)s')
+
+class Friendship:
+    """A simple class"""
+
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self._age = age
+
+        logging.info("Friend Name-Age: {} - {}".format(self.fullname, self._age))
+
+    @property
+    def age(self):
+        return "{}.".format(self._age)
+
+    @property
+    def fullname(self):
+        return "{} {}.".format(self.first, self.last)
+
+
+friend1 = Friendship("Yunus", "Emre", 31)
+friend2 = Friendship("Efe", "Goalkeeper", 10)
+friend3 = Friendship("Mr", "Özdemir", 20)
